a43cd411b72a71d455fbfc1c1e1f246365714a97
yanieznaio <juliette.or@icloud.com>
2024-03-18 22:20:15 +0100

    modif

--- app/compenents/button/ButtonLink.js
+++ app/compenents/button/ButtonLink.js
@@ -6,7 +6,7 @@ const ButtonLink = ({ href, title, dark }) => {
     const [over, setOver] = useState(false)
 
     return (
-        <Link onMouseOver={() => setOver(true)} onMouseLeave={() => setOver(false)} href={href} className='relative flex items-center justify-center px-2 py-2 text-xs rounded-full cursor pointer font-mori' >
+        <Link onMouseOver={() => setOver(true)} onMouseLeave={() => setOver(false)} href={href} className='relative flex items-center justify-center px-4 py-2 text-xs rounded-full md:px-8 md:text-lg cursor pointer font-mori' >
             <div className={`absolute z-10 w-full p-2 md:p-6 h-full ${dark ? "bg-dark-brown" : "bg-white"} rounded-full ${over ? 'scale-[1.1]' : 'scale-1'}  transition-all ease-in-out`}>
 
             </div>
--- app/compenents/hero/HeroBox.js
+++ app/compenents/hero/HeroBox.js
@@ -3,7 +3,7 @@ import ButtonLink from '../button/ButtonLink'
 
 const HeroBox = () => {
     return (
-        <div className='absolute flex flex-col md:w-[30vw] max-w-[483px] min-w-[350px]  md:max-h-[650px] gap-8  p-4 backdrop-filter bg-light-brown backdrop-blur-xl bg-opacity-10  h-[75vh] md:h-[85vh] mt-12 '>
+        <div className='absolute flex flex-col md:w-[30vw] max-w-[483px] min-w-[350px]  max-h-[500px] md:max-h-[650px] gap-8  p-4 backdrop-filter bg-light-brown backdrop-blur-xl bg-opacity-10  h-[75vh] md:h-[85vh] mt-12 '>
             <div className="bg-[url('/house1.png')] bg-center bg-cover bg-no-repeat   h-[70%] md:h-2/3 w-full flex justify-center items-end py-6">
                 <ButtonLink href='/contact' title='Contact' />
 
