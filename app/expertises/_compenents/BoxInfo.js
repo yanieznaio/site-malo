ea04035d41e4bc4885d79490aa262c8f372f0843
yanieznaio <juliette.or@icloud.com>
2024-06-01 12:03:24 +0200

    modif

--- app/expertises/_compenents/BoxInfo.js
+++ app/expertises/_compenents/BoxInfo.js
@@ -24,30 +24,30 @@ const BoxInfo = ({ content }) => {
                                     </div>
                                 </div>
                                 <div className="absolute bottom-0 right-0 max-w-xs lg:-mb-16">
-                                    <div className="px-6 pt-6 pb-6 lg:pr-16 bg-[#F6F5F2] rounded-3xl shadow-xl">
+                                    <div className="px-6 pt-6 pb-6 lg:pr-16 bg-[#F6F5F2] rounded-3xl shadow-xl flex flex-col">
                                         <div className="inline-flex mb-6">
-                                            <div className="flex items-center justify-center w-12 h-12 bg-green-200 rounded-full">
+                                            <div className="flex items-center justify-center w-8 h-8 rounded-full bg-green-50 md:w-12 md:h-12">
                                                 <img src="/iconEnergy.png" alt="" />
                                             </div>
-                                            <div className="mx-3">
+                                            <div className="hidden mx-3 md:block">
                                                 <span className="text-sm font-semibold">Etude thermique</span>
                                                 <span className="block text-xs text-gray-500">Des expertises adaptés a vos besoins</span>
                                             </div>
                                         </div>
-                                        <div className="inline-flex mb-6">
-                                            <div className="flex items-center justify-center w-12 h-12 bg-blue-200 rounded-full">
+                                        <div className="inline-flex mb-6 ">
+                                            <div className="flex items-center justify-center w-8 h-8 rounded-full bg-green-50 md:w-12 md:h-12">
                                                 <img src="/iconEco.png" alt="" />
                                             </div>
-                                            <div className="mx-3">
+                                            <div className="hidden mx-3 md:block">
                                                 <span className="text-sm font-semibold">Conception bas carbonne</span>
                                                 <span className="block text-xs text-gray-500"></span>
                                             </div>
                                         </div>
                                         <div className="inline-flex">
-                                            <div className="flex items-center justify-center w-12 h-12 bg-green-200 rounded-full">
+                                            <div className="flex items-center justify-center w-8 h-8 rounded-full bg-green-50 md:w-12 md:h-12">
                                                 <img src="/iconFile.png" alt="" />
                                             </div>
-                                            <div className="mx-3">
+                                            <div className="hidden mx-3 md:block">
                                                 <span className="text-sm font-semibold">Conseils&accompagnement</span>
                                                 <span className="block text-xs text-gray-500">Nous vous aidons a réaliser vos projets</span>
                                             </div>
